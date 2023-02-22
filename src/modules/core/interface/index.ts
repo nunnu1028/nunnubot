c82aa6cd4cc4c7b259804768c62ca8d3a6536bcb
nunnu1028 <dev.nunnu1028@gmail.com>
2023-02-22 21:27:19 +0900

    I don't use kakaolink anymore.

--- src/modules/core/interface/index.ts
+++ src/modules/core/interface/index.ts
@@ -1,2 +1,1 @@
 export * from "./function-result";
-export * from "./secretInfo";
--- src/modules/core/interface/secretInfo.ts
+++
@@ -1,8 +0,0 @@
-export interface SecretInfo {
-	kakaoLink: {
-		email: string;
-		password: string;
-		key: string;
-		url: string;
-	};
-}
